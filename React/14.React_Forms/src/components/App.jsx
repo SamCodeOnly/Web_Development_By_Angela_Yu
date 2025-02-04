81abfe8f2ec947b9ca8548ba0af3ee3c5ef223b3
samcodeonly <samcodeonly@gmail.com>
2025-02-04 12:29:13 +0300

    event_handling

---
+++ React/14.React_Forms/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>Hello</h1>
+      <input type="text" placeholder='what your name?' />
+      <button>Submit</button>
+    </div>
+  )
+}
+
+export default App
