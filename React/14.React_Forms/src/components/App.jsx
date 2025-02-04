4a4780c026c50dafb89722cfbeab5aad541112e0
samcodeonly <samcodeonly@gmail.com>
2025-02-04 12:33:11 +0300

    eventHandling

--- React/14.React_Forms/src/components/App.jsx
+++ React/14.React_Forms/src/components/App.jsx
@@ -2,10 +2,14 @@ import React, { useState } from 'react'
 
 
 function App() {
+
+  function handleChange() {
+    
+  }
   return (
     <div className='container'>
       <h1>Hello</h1>
-      <input type="text" placeholder='what your name?' />
+      <input onChange={handleChange} type="text" placeholder='what your name?' />
       <button>Submit</button>
     </div>
   )
